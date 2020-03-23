04f69b3e0c7982ce1e9e43173acd9966a3008c15
aiperieldar <aiperieldar.dev@gmail.com>
2020-03-22 20:04:15 -0400

    Initial Commit

--- app/routes.js
+++ app/routes.js
@@ -18,6 +18,15 @@ module.exports = function(app, passport, db) {
         })
     });
 
+    app.get('/personal', isLoggedIn, function(req, res) {
+        db.collection('messages').find().toArray((err, result) => {
+          if (err) return console.log(err)
+          res.render('personal.ejs', {
+            user : req.user,
+            messages: result
+          })
+        })
+    });
     // LOGOUT ==============================
     app.get('/logout', function(req, res) {
         req.logout();
@@ -33,6 +42,57 @@ module.exports = function(app, passport, db) {
         res.redirect('/profile')
       })
     })
+
+
+    app.put('/messages', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbUp:req.body.thumbUp + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.put('/thumbDown', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbUp:req.body.thumbUp - 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.delete('/messages', (req, res) => {
+      console.log('delete')
+      db.collection('messages').findOneAndDelete({name: req.body.name}, (err, result) => {
+        if (err) return res.send(500, err)
+        res.send('Message deleted!')
+      })
+    })
+
+
+
+
+    app.post('/messages', (req, res) => {
+      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/personal')
+      })
+    })
+
 
     app.put('/messages', (req, res) => {
       db.collection('messages')
